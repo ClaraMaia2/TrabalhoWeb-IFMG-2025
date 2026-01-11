dcf205cf7522d255b45f4634552593aedace3f16
Clara <claraemarina80@gmail.com>
2026-01-11 19:25:42 -0300

    feat: created necessary views for Accounts, Appointments and Core apps

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
-# Create your views here.
+def sign_up(request):
+    if request.method == 'POST':
+        user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'], email=request.POST['email'])
+        
+        Profile.objects.create(
+            user=user, 
+            cpf=request.POST['cpf'], 
+            sexo=request.POST['sexo'], 
+            data_nascimento=request.POST['data_nascimento'], 
+            celular=request.POST['celular']
+        )
+        
+        login(request, user)
+        
+        return redirect('home')
+    #endif
+    
+    return render(request, 'accounts/signup.html')
+#enddef
+
+@login_required
+def profile_view(request):
+    return render(request, 'accounts/profile.html')
+#enddef
--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from datetime import datetime
+from .models import Appointment, Service
+from .utils import AVAILABLE_TIMES
 
-# Create your views here.
+@login_required
+def create_appointment(request):
+    services = Service.objects.all()
+    available_times = AVAILABLE_TIMES
+    
+    if request.method == 'POST':
+        date_appointment = request.POST['date_appointment']
+        time_appointment = request.POST['time_appointment']
+        service_id = request.POST['service']
+        
+        Appointment.objects.create(
+            client=request.user,
+            service_id=service_id,
+            date_appointment=date_appointment,
+            time_appointment=time_appointment
+        )
+        
+        return redirect('my_appointments')
+    #enddef
+    
+    selected_date = request.GET.get('date_appointment')
+    
+    if selected_date:
+        occupied = Appointment.objects.filter(date_appointment=selected_date).values_list('time', flat=True)
+        
+        occupied = [t.strftime('%H:%M') for t in occupied]
+        
+        available_times = [h for h in AVAILABLE_TIMES not in occupied]
+    #endif
+    
+    return render(request, 'appointments/make_appointment.html', {'services': services, 'available_times': available_times})
+#enddef
+
+@login_required
+def my_appointments(request):
+    appointments = Appointment.objects.filter(client=request.user)
+    
+    return render(request, 'appointments/my_appointments.html', {'appointments': appointments})
+#enddef
+
+@staff_member_required
+def all_appointments(request):
+    appointments = Appointment.objects.all()
+    
+    return render(request, 'appointments/all_appoinments.html', {'appointments': appointments})
+#enddef
