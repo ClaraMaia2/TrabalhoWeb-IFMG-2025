e4eb3d55e002f2c86cf4cb148d9436fbd331359e
Clara <claraemarina80@gmail.com>
2026-01-12 15:20:49 -0300

    feat: add login and signup templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,27 +1,34 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from django.db import transaction
+from .forms import ProfileForm, SignupForm
 
 def sign_up(request):
     if request.method == 'POST':
-        user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'], email=request.POST['email'])
+        user_form = SignupForm(request.POST)
+        profile_form = ProfileForm(request.POST)
         
-        Profile.objects.create(
-            user=user, 
-            cpf=request.POST['cpf'], 
-            sexo=request.POST['sexo'], 
-            data_nascimento=request.POST['data_nascimento'], 
-            celular=request.POST['celular']
-        )
-        
-        login(request, user)
-        
-        return redirect('home')
+        if user_form.is_valid() and profile_form.is_valid():
+            with transaction.atomic():
+                user = user_form.save()
+                
+                profile = profile_form.save(commit=False)
+                profile.user = user
+                profile.save()
+                
+                login(request, user)
+                
+                return redirect('core:home')
+            #endwith
+        #endif
     #endif
+    else:
+        user_form = SignupForm()
+        profile_form = ProfileForm()
+    #endelse
     
-    return render(request, 'accounts/signup.html')
+    return render(request, 'accounts/signup.html', {'user_form': user_form, 'profile_form': profile_form,})
 #enddef
 
 @login_required
--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'core:home'
+LOGOUT_REDIRECT_URL = 'core:home'
+LOGIN_URL = 'accounts:login'
